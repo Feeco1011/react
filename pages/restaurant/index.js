63827d677e42b7d69eb20e2f2dc91b8ef4bff844
john-1812 <john1812wtfffff@gmail.com>
2024-11-05 16:36:16 +0800

    新增餐廳列表的路由

---
+++ pages/restaurant/index.js
@@ -0,0 +1,543 @@
+import React from 'react'
+import style from '@/styles/restaurantPage.module.css'
+
+export default function Index() {
+  return (
+    <>
+    <div>餐廳列表頁面</div>
+    <header className={style.header}>
+      <div className={style.logo}>
+        <img src="/logoTest/logoTest-removebg-preview.png" alt="Logo" />
+      </div>
+      <div className={style.headerLinks}>
+        <a href="" className="headerLinks link">註冊</a>
+        <a href="" className="headerLinks link">登入</a>
+      </div>
+    </header>
+
+    <div className="container">
+      <div className="cardContainer" id="cardContainer1">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer2">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer3">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer4">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+      <div className="cardContainer" id="cardContainer5">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer6">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer7">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer8">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer9">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer10">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer11">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer12">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer13">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer14">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer15">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainer" id="cardContainer16">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+    </div>
+
+    <div className="largeContainer">
+      <div className="cardContainerLarge" id="cardContainerLarge1">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainerLarge" id="cardContainerLarge2">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainerLarge" id="cardContainerLarge3">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+
+      <div className="cardContainerLarge" id="cardContainerLarge4">
+        <div className="image-section">
+          <img
+            src="/restaurantPicture/one/心潮1.jpg"
+            alt="Photo"
+            className="photo"
+          />
+          <div className="overlay">
+            <img src="/logoTest/logoTest.ico" alt="icon" className="icon" />
+            <span className="text">文字標題</span>
+          </div>
+          <div className="right-icon-overlay">
+            <img
+              src="/logoTest/logoTest.ico"
+              alt="icon"
+              className="right-icon"
+            />
+          </div>
+        </div>
+        <div className="text-section">
+          <p>這是第一行文字</p>
+          <p>這是第二行文字</p>
+        </div>
+      </div>
+    </div>
+
+    <div className="rectangle-container">
+      <div className="rectangle rectangle1">
+          <div className="rectangle-text">文本1</div>
+      </div>
+      <div className="rectangle rectangle2">
+          <div className="rectangle-text">文本2</div>
+      </div>
+      <div className="rectangle rectangle3">
+          <div className="rectangle-text">文本3</div>
+      </div>
+      <div className="rectangle rectangle4">
+          <div className="rectangle-text">文本4</div>
+      </div>
+      <div className="rectangle rectangle5">
+          <div className="rectangle-text">文本5</div>
+      </div>
+      <div className="rectangle rectangle6">
+          <div className="rectangle-text">文本6</div>
+      </div>
+  </div>
+  </>
+  )
+}
