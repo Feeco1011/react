c7549cf41ee74ea65e2dd62f3d912bc6750e708a
mou <super82621800118@gmail.com>
2024-11-15 15:42:18 +0800

    V13

--- pages/index.js
+++ pages/index.js
@@ -4,90 +4,92 @@ import { Inter } from 'next/font/google'
 import style from '@/styles/home.module.css'
 import styles from '@/styles/frame.module.css'
 
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-    <div className={styles.div}>
-      <div className={styles.title}>玩遍台灣，吃遍美味，住進美好時光！</div>
-      <div className={styles.pics}>
-        <img className={styles.div2} alt="" src="/hotel.jpg" />
-        <img className={styles.div4} alt="" src="/food.jpg" />
-        <div className={styles.textParent}>
-          <img className={styles.christmas} alt="" src="/trip.jpg" />
+      <div className={styles.div}>
+        <div className={styles.title}>玩遍台灣，吃遍美味，住進美好時光！</div>
+        <div className={styles.pics}>
+          <img className={styles.div2} alt="" src="/hotel.jpg" />
+          <img className={styles.div4} alt="" src="/food.jpg" />
+          <div className={styles.textParent}>
+            <img className={styles.christmas} alt="" src="/trip.jpg" />
+          </div>
         </div>
-      </div>
-      <div className={styles.icons}>
-        <div className={styles.div5}>
-          <a href="index123">
-            <img className={styles.dish1Icon} alt="" src="/house.png" />
-            <b className={styles.b5}>住宿</b>
-          </a>
-        </div>
-        <div className={styles.div5}>
-          <a href="index123">
-            <img className={styles.dish1Icon} alt="" src="/dish.png" />
-            <b className={styles.b5}>餐廳</b>
-          </a>
-        </div>
-        <div className={styles.div5}>
-          <a href="index123">
-            <img className={styles.dish1Icon} alt="" src="/travel.png" />
-            <b className={styles.b5}>景點</b>
-          </a>
-        </div>
-        <div className={styles.div5}>
-          <a href="index123">
-            <img className={styles.dish1Icon} alt="" src="/speech.png" />
-            <b className={styles.b5}>論壇</b>
-          </a>
-        </div>
-        <div className={styles.div5}>
-          <a href="index123">
-            <img className={styles.dish1Icon} alt="" src="/camping-tent.png" />
-            <b className={styles.b5}>露營</b>
-          </a>
-        </div>
-        <div className={styles.div5}>
-          <a href="index123">
-            <img className={styles.dish1Icon} alt="" src="/shrine.png" />
-            <b className={styles.b5}>宮廟參拜</b>
-          </a>
-        </div>
-      </div>
-    </div>
-      <div className={style.container}>
-          <div className={style.cardContainer} id="cardContainer1" >
-            <div className={style.imageSection}>
+        <div className={styles.icons}>
+          <div className={styles.div5}>
+            <a href="index123">
+              <img className={styles.dish1Icon} alt="" src="/house.png" />
+              <b className={styles.b5}>住宿</b>
+            </a>
+          </div>
+          <div className={styles.div5}>
+            <a href="index123">
+              <img className={styles.dish1Icon} alt="" src="/dish.png" />
+              <b className={styles.b5}>餐廳</b>
+            </a>
+          </div>
+          <div className={styles.div5}>
+            <a href="index123">
+              <img className={styles.dish1Icon} alt="" src="/travel.png" />
+              <b className={styles.b5}>景點</b>
+            </a>
+          </div>
+          <div className={styles.div5}>
+            <a href="index123">
+              <img className={styles.dish1Icon} alt="" src="/speech.png" />
+              <b className={styles.b5}>論壇</b>
+            </a>
+          </div>
+          <div className={styles.div5}>
+            <a href="index123">
               <img
-                src="/restaurantPicture/one/心潮1.jpg"
-                alt="Photo"
-                className={style.photo}
+                className={styles.dish1Icon}
+                alt=""
+                src="/camping-tent.png"
               />
-              <div className={style.overlay}>
-                <img
-                  src="/logoTest/logoTest.ico"
-                  alt="icon"
-                  className={style.icon}
-                />
-                <span className={style.text}>文字標題</span>
-              </div>
-              <div className={style.rightIconOverlay}>
-                <img
-                  src="/logoTest/logoTest.ico"
-                  alt="icon"
-                  className={style.rightIcon}
-                />
-              </div>
+              <b className={styles.b5}>露營</b>
+            </a>
+          </div>
+          <div className={styles.div5}>
+            <a href="index123">
+              <img className={styles.dish1Icon} alt="" src="/shrine.png" />
+              <b className={styles.b5}>宮廟參拜</b>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={style.container}>
+        <div className={style.cardContainer} id="cardContainer1">
+          <div className={style.imageSection}>
+            <img
+              src="/restaurantPicture/one/心潮1.jpg"
+              alt="Photo"
+              className={style.photo}
+            />
+            <div className={style.overlay}>
+              <img
+                src="/logoTest/logoTest.ico"
+                alt="icon"
+                className={style.icon}
+              />
+              <span className={style.text}>文字標題</span>
             </div>
-            <div className={style.textSection}>
-              <p>這是第一行文字</p>
-              <p>這是第二行文字</p>
+            <div className={style.rightIconOverlay}>
+              <img
+                src="/logoTest/logoTest.ico"
+                alt="icon"
+                className={style.rightIcon}
+              />
             </div>
           </div>
-        
+          <div className={style.textSection}>
+            <p>這是第一行文字</p>
+            <p>這是第二行文字</p>
+          </div>
+        </div>
 
         <div className={style.cardContainer} id="cardContainer2">
           <div className={style.imageSection}>
@@ -524,8 +526,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-      
 
       <div className={style.rectangleContainer}>
         <div className={`${style.rectangle} ${style.rectangle1}`}>
