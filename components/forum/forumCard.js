00e9c42a2c1cdf671136a02488d5609626a9dbdf
togenkin <togenkin@gmail.com>
2024-11-19 09:57:23 +0800

    test

--- components/forum/forumCard.js
+++ components/forum/forumCard.js
@@ -4,14 +4,29 @@ import styles from '@/styles/new.module.css'
 export default function ForumCard(props) {
   // 檢查傳入的是哪種類型的數據
   const cardData = props.view || props.category || props.data || {}
-  
+
+  // 動態選擇資料夾和圖片路徑
+  const folder = props.view
+    ? 'viewblog-img'
+    : props.category
+    ? 'category-img'
+    : props.data
+    ? 'data-img'
+    : ''
+
+  const imagePath = folder
+    ? `/${folder}/${cardData?.image || 'default.jpg'}`
+    : '/default.jpg'
+
   // 使用可選鏈運算符來安全地訪問數據
   const userData = {
     user: cardData?.user || '未知用戶',
     createdDate: cardData?.pt_created || '未知日期',
     city: cardData?.pt_city || '未知城市',
     description: cardData?.pt_sf || '暫無描述',
-    image: cardData?.pt_img || 'https://media.istockphoto.com/id/1502547874/zh/%E5%90%91%E9%87%8F/travel-to-world-on-banner-background-landscape-template-tourism-cards-road-trip-vacation-as-in.jpg?s=1024x1024&w=is&k=20&c=WJSv71le86s6btsNKlRoMpQP98r1T68Ux7P7B8zo1lo='
+    image:
+      cardData?.pt_img ||
+      'https://media.istockphoto.com/id/1502547874/zh/%E5%90%91%E9%87%8F/travel-to-world-on-banner-background-landscape-template-tourism-cards-road-trip-vacation-as-in.jpg?s=1024x1024&w=is&k=20&c=WJSv71le86s6btsNKlRoMpQP98r1T68Ux7P7B8zo1lo=',
   }
 
   // 添加數據檢查
@@ -26,7 +41,8 @@ export default function ForumCard(props) {
           src={userData.image}
           alt="景點圖片"
           onError={(e) => {
-            e.target.src = 'https://via.placeholder.com/300x200?text=圖片載入失敗'
+            e.target.src =
+              'https://via.placeholder.com/300x200?text=圖片載入失敗'
           }}
         />
 
